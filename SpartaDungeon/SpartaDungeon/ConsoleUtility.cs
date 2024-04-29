ca021a768a9c934ee7ea0f9e6fe5198f136db947
SooHyeon Kim <tngusrla0812@gmail.com>
2024-04-29 15:49:20 +0900

    ADD > Scene+Utility
    씬 구성과 이동, 유틸리티 함수를 구현했습니다.

---
+++ SpartaDungeon/SpartaDungeon/ConsoleUtility.cs
@@ -0,0 +1,59 @@
+﻿
+using System.Runtime.CompilerServices;
+
+internal class ConsoleUtility
+{
+    public static int PromptMenuChoice(int min, int max)
+    {
+        while (true)
+        {
+            Console.WriteLine("  원하시는 번호를 입력해주세요.");
+            Console.WriteLine("  >> ");
+            if (int.TryParse(Console.ReadLine(), out var choice) && choice >= min && choice <= max)
+            {
+                return choice;
+            }
+            Console.WriteLine("  잘못된 입력입니다. 다시 시도해주세요.");
+        }
+    }
+    internal static void ShowTitle(string title)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(title);
+        Console.ResetColor();
+    }
+
+    public static int GetPrintableLength(string str)
+    {
+        int length = 0;
+        foreach (char c in str)
+        {
+            if (char.GetUnicodeCategory(c) == System.Globalization.UnicodeCategory.OtherLetter)
+            {
+                length += 2; // 한글과 같은 넓은 문자에 대해 길이를 2로 취급
+            }
+            else
+            {
+                length += 1; // 나머지 문자에 대해 길이를 1로 취급
+            }
+        }
+        return length;
+    }
+
+    public static void PrintTextHighlights(string s1, string s2, string s3 = "")
+    {
+        Console.WriteLine(s1);
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(s2);
+        Console.WriteLine();
+        Console.WriteLine(s3);
+    }
+
+    public static void HeightPadding()
+    {
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+}
---
+++ SpartaDungeon/SpartaDungeon/GameScene.cs
@@ -0,0 +1,138 @@
+﻿
+public class GameScene
+{
+    public void StartView()
+    {
+        Console.Clear();
+        ConsoleUtility.HeightPadding();
+
+        Console.WriteLine(string.Format("{0}", "스파르타 던전").PadLeft(42 - (21 - ("스파르타 던전".Length / 2))));
+
+        ConsoleUtility.HeightPadding();
+
+        // 이름 물어보기
+        Console.Write(string.Format("{0}", "닉네임 : ").PadLeft(42 - (29 - ("닉네임 : ".Length / 2))));
+        Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        // 직업
+        Console.Write(string.Format("{0}", "1. 전사  |  2. 마법사 ").PadLeft(42 - (19 - ("1. 전사  |  2. 마법사 ".Length / 2))));
+        Console.WriteLine();
+        Console.WriteLine();
+        // 직업 고르기
+        Console.Write(string.Format("{0}", "캐릭터 선택 : ").PadLeft(42 - (27 - ("캐릭터 선택 : ".Length / 2))));
+        Console.ReadLine();
+        Console.WriteLine();
+
+        ConsoleUtility.HeightPadding();
+
+        Console.WriteLine("=================================================");
+        Console.WriteLine("              PRESS ANYKEY TO START              ");
+        Console.WriteLine("=================================================");
+        Console.ReadKey();
+        MainView();
+    }
+
+    public void MainView()
+    {
+        Console.Clear();
+        Console.WriteLine();
+
+        Console.WriteLine("  스파르타 마을에 오신 여러분 환영합니다.");
+        Console.WriteLine("  이제 전투를 시작할 수 있습니다. ");
+        ConsoleUtility.HeightPadding();
+
+        Console.WriteLine("  1. 상태 보기");
+        Console.WriteLine("  2. 전투 시작");
+        ConsoleUtility.HeightPadding();
+
+        // 선택한 결과를 검증함
+        int choice = ConsoleUtility.PromptMenuChoice(1, 2);
+
+        // 선택한 결과에 따라 보내줌
+        switch (choice)
+        {
+            case 1:
+                StatusView();
+                break;
+            case 2:
+                BattleView();
+                break;
+        }
+        MainView();
+    }
+
+
+    private void StatusView()
+    {
+        Console.Clear();
+        Console.WriteLine();
+
+        // 제목 색 다름
+        ConsoleUtility.ShowTitle("  상태 보기");
+        Console.WriteLine("  캐릭터의 정보가 표기됩니다.");
+        ConsoleUtility.HeightPadding();
+
+        Console.WriteLine($"  LV. {0}"); //player.Level
+        Console.WriteLine($"  {0} ({1})"); //player.Name, player.Job
+        Console.WriteLine($"  공격력 : {0}"); //player.Attack
+        Console.WriteLine($"  방어력 :  {0}"); //player.Defense
+        Console.WriteLine($"  Golde : {0}"); //player.Gold
+        ConsoleUtility.HeightPadding();
+
+        Console.WriteLine("  0. 나가기");
+        Console.WriteLine();
+
+        // 선택한 결과를 검증함
+        int choice = ConsoleUtility.PromptMenuChoice(0, 0);
+
+        // 선택한 결과에 따라 보내줌
+        switch (choice)
+        {
+            case 0:
+                MainView();
+                break;
+        }
+        StatusView();
+    }
+
+    private void BattleView()
+    {
+        Console.Clear();
+        Console.WriteLine();
+
+        // 제목 색 다름
+        ConsoleUtility.ShowTitle("  던전");
+        Console.WriteLine("  몬스터를 해치우고 전리품을 획득하세요.");
+        ConsoleUtility.HeightPadding();
+
+        Console.WriteLine($"  LV.{0} {1} HP {2}"); //미니언.Level 미니언.Name 미니언.HP
+        Console.WriteLine($"  LV.{0} {1} HP {2}"); //대포미니언.Level 대포미니언.Name 대포미니언.HP
+        Console.WriteLine($"  LV.{0} {1} HP {2}"); //공허충.Level 공허충.Name 공허충.HP
+        ConsoleUtility.HeightPadding();
+
+        Console.WriteLine("  [내정보]");
+        Console.WriteLine($"  LV.{0} {1}({2})"); //player.Level player.Name player.Job
+        Console.WriteLine($"  HP {0}/{1}"); //player.MaxHp player.CurrentHP
+        ConsoleUtility.HeightPadding();
+
+        Console.WriteLine("  0. 나가기");
+        Console.WriteLine("  1. 공격");
+        Console.WriteLine();
+
+        // 선택한 결과를 검증함
+        int choice = ConsoleUtility.PromptMenuChoice(0, 1);
+
+        // 선택한 결과에 따라 보내줌
+        switch (choice)
+        {
+            case 0:
+                MainView();
+                break;
+            case 1:
+                //Battle();
+                break;
+        }
+        BattleView();
+    }
+}
