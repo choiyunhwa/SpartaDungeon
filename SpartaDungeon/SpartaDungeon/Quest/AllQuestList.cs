5cb3b8ba6d251152c40703cc60b15100dd42fff2
KwonSinwook <1005good@naver.com>
2024-04-30 15:54:25 +0900

    Add > AllQuestList
    일단 모든 퀘스트가 들어가 있는 AllQuestList를 추가했습니다.

---
+++ SpartaDungeon/SpartaDungeon/Quest/AllQuestList.cs
@@ -0,0 +1,36 @@
+﻿using SpartaDungeon.Quest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class AllQuestList
+{
+    KillDragon KillDragon { get; set; }
+    KillMinion killMinion { get; set; }
+    KillSeigeMinion killseigeMinion { get; set; }
+    KillVoidling killVoidling { get; set; }
+    TryEquipment tryEquipment { get; set; }
+    TryLevelUp tryLevelUp { get; set; }
+    List<IQuest> questsList { get; set; }
+
+    public AllQuestList()
+    {
+        KillDragon = new KillDragon();
+
+        killMinion = new KillMinion();
+
+        killseigeMinion = new KillSeigeMinion();
+
+        killVoidling = new KillVoidling();
+
+        tryEquipment = new TryEquipment();
+
+        tryLevelUp = new TryLevelUp();
+
+        questsList = new List<IQuest>() { KillDragon, killMinion, killseigeMinion, killVoidling, tryEquipment, tryLevelUp};
+    }
+
+}
